2c15544ddb8ebb5f41b54c8ead9fffd2e4d140a9
rto24 <richardto180@gmail.com>
2024-12-14 19:40:08 -0500

    imported auth router and set prefix to route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes"
 
 dotenv.config();
 
@@ -9,5 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
 
 export default app;
