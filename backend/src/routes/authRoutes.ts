c55b4079eea1cedc2755d0d16e029c8f70c15ac8
rto24 <richardto180@gmail.com>
2024-12-14 19:41:33 -0500

    auth routes

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,5 +1,15 @@
-import express from "express";
+import express, { Response, Request } from "express";
+import { registerUser, loginUser, refreshToken } from "../controllers/authController";
+import { authenticateJWT } from "../middleware/authMiddleware";
+import { JwtPayload } from "jsonwebtoken";
 
 const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/refresh", refreshToken);
+router.get("/protected", authenticateJWT, (req: Request, res: Response) => {
+  res.json({ message: "Protected route accessed", user: req.user });
+})
 
 export default router;
