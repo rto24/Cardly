d71aee0d6d32011a40ec8138f023de3e08429710
rto24 <richardto180@gmail.com>
2024-12-14 19:42:15 -0500

    updated id values due to changing db schema from string to int

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -6,12 +6,12 @@ if (!JWT_SECRET) {
   throw new Error("JWT_SECRET must be defined in environment variables.");
 };
 
-export const generateAccessToken = (userId: string) => {
-  return jwt.sign({ id: userId }, JWT_SECRET, { expiresIn: "1h" });
+export const generateAccessToken = (userId: number) => {
+  return jwt.sign({ id: userId.toString() }, JWT_SECRET, { expiresIn: "1h" });
 };
 
-export const generateRefreshToken = (userId: string) => {
-  return jwt.sign({ id: userId }, JWT_SECRET, { expiresIn: "7d"});
+export const generateRefreshToken = (userId: number) => {
+  return jwt.sign({ id: userId.toString() }, JWT_SECRET, { expiresIn: "7d"});
 };
 
 export const verifyToken = (token: string) => {
