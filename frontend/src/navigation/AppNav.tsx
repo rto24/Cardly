f8b3758bda1a895fc704e37c38852f46db103dca
rto24 <richardto180@gmail.com>
2024-12-17 18:30:13 -0500

    added login screen for initial nav route

---
+++ frontend/src/navigation/AppNav.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import LoginScreen from "../screens/LoginScreen";
+
+const Stack = createStackNavigator();
+
+const AppNav = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen 
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default AppNav;
