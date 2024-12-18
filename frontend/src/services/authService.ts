e184ce9ea14a60beece5b9e20f92801d7b551cb1
rto24 <richardto180@gmail.com>
2024-12-18 18:34:49 -0500

    added register user service

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -13,11 +13,38 @@ export const loginUser = async (email: string, password: string) => {
       })
     });
 
+    const data = response.json();
+
     if (!response.ok) {
       throw new Error("Failed to log user in");
     }
-    return response.json();
+    return data;
   } catch (error) {
     console.error("Failed to log user in:", error)
+  }
+};
+
+export const registerUser = async (email: string, username: string, password: string) => {
+  try {
+    const response = await fetch(`${API_URL}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        username,
+        password
+      })
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+    return data;
+  } catch (error) {
+    console.error("Failed to sign up user:", error);
   }
 };
