5d8930d3c2e334d8d8a2187d476a65793ab19f13
rto24 <richardto180@gmail.com>
2024-12-19 18:20:06 -0500

    added route to get user posts

--- backend/src/controllers/postController.ts
+++ backend/src/controllers/postController.ts
@@ -17,3 +17,16 @@ export const createPost = async (req: Request, res: Response): Promise<void> => 
     res.status(500).json({ message: "Error creating post:", error });
   }
 };
+
+export const getUserPosts = async (req: Request, res: Response): Promise<void> => {
+  const { userId } = req.params;
+  try {
+    const numericId = +userId;
+    const userPosts = await prisma.post.findMany({ where: { userId: numericId } });
+
+    res.status(200).json(userPosts);
+  } catch (error) { 
+    console.error("Error fetching user posts:", error);
+    res.status(500).json({ message: "Error getting user posts:", error});
+  }
+};
