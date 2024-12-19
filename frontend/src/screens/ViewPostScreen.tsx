94e6bbc421a2e9f0b37edbc26df531757d06e299
rto24 <richardto180@gmail.com>
2024-12-19 18:04:49 -0500

    fetched user posts

--- frontend/src/screens/ViewPostScreen.tsx
+++ frontend/src/screens/ViewPostScreen.tsx
@@ -1,10 +1,33 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { Posts } from "../types/types";
+import { getUserPosts } from "../services/postService";
+import { useAuth } from "../context/AuthContext";
 
 const ViewPostsScreen = () => {
+  const { user } = useAuth();
+  const [ posts, setPosts ] = useState<Posts[]>([]);
+
+  useEffect(() => {
+    const fetchUserPosts = async (userId: number) => {
+      try {
+        const response = await getUserPosts(userId);
+        const data: Posts[] = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.error("Failure fetching user posts:", error);
+      }
+    }
+    fetchUserPosts(user);
+  }, []);
+
   return (
     <View>
-      <Text>YOUR POSTS ARE HERE</Text>
+      {/* <FlatList 
+        data={posts}
+        renderItem={({post}) => } //add post component here
+        keyExtractor={post => post.id}
+      /> */}
     </View>
   )
 }
