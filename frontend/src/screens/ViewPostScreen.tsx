8071bbc1b23de561ae49b0146c48a4078c5b3b1a
rto24 <richardto180@gmail.com>
2024-12-19 16:24:19 -0500

    added screen to let user look at their posts

---
+++ frontend/src/screens/ViewPostScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ViewPostsScreen = () => {
+  return (
+    <View>
+      <Text>YOUR POSTS ARE HERE</Text>
+    </View>
+  )
+}
+
+export default ViewPostsScreen;
