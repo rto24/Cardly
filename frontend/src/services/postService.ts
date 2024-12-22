48ac657d68a473d03071c4b41b3af87c261f5b11
rto24 <richardto180@gmail.com>
2024-12-22 01:18:42 -0500

    added method for comments and likes

--- frontend/src/services/postService.ts
+++ frontend/src/services/postService.ts
@@ -19,3 +19,52 @@ export const getUserPosts = async (userId: number) => {
     console.error("Failed to get user posts:", error);
   }
 };
+
+export const likePost = async (postId: number, userId: number) => {
+  try {
+    const response = await fetch(`${API_URL}/${postId}/like`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        postId,
+        userId
+      })
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error("Failed to like post");
+    }
+    return data;
+  } catch (error) {
+    console.error("Failed to like post:", error)
+  }
+};
+
+export const commentOnPost = async (postId: number, userId: number, content: string) => {
+  try {
+    const response = await fetch(`${API_URL}/${postId}/comment`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        postId,
+        userId,
+        content
+      })
+    });
+
+    const data = response.json();
+    
+    if (!response.ok) {
+      throw new Error("Failed to comment on post");
+    }
+    return data;
+  } catch (error) {
+    console.error("Failed to comment on post:", error);
+  }
+};
