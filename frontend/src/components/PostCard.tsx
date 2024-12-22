9eba24d5efbef35260a5b200fc9662ce2c857732
rto24 <richardto180@gmail.com>
2024-12-22 00:12:41 -0500

    added modal for comments section

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { TextInput, View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { TextInput, View, Image, Text, TouchableOpacity, Modal, ScrollView, StyleSheet } from "react-native";
 import { Posts } from "../types/types";
 
 const PostCard: React.FC<Posts> = ({
@@ -14,28 +14,57 @@ const PostCard: React.FC<Posts> = ({
   onLike,
   onComment,
   createdAt,
-}) => (
-  <View>
-    <View>
-      <Image source={{ uri: userAvatar }}/>
-      <Text>{username}</Text>
-    </View>
-    {imageUrl &&
-      <Image source={{ uri: imageUrl }}/>
-    }
-    <View>
-      <TouchableOpacity onPress={() => onLike(id)}>
-        <Text>♡</Text>
-      </TouchableOpacity>
-    </View>
-    <Text>{title}</Text>
-    <Text>{content}</Text>
-  </View>
-); 
+}) => {
+  
+  const [ isCommentModalOpen, setIsCommentModalOpen ] = useState<boolean>(false);
+  const [ isLikeModalOpen, setIsLikeModalOpen ] = useState<boolean>(false);
 
-//need to add modal for comments
-  //map comments to display in modal
-//need to add modal for user likes
-  //map likes to display users that liked the post
+  const openComments = () => setIsCommentModalOpen(!isCommentModalOpen);
+  const openLikes = () => setIsLikeModalOpen(!isLikeModalOpen);
+
+  return (
+    <View>
+      <View>
+        <Image source={{ uri: userAvatar }}/>
+        <Text>{username}</Text>
+      </View>
+      {imageUrl &&
+        <Image source={{ uri: imageUrl }}/>
+      }
+      <View>
+        <TouchableOpacity onPress={() => onLike(id)}>
+          <Text>♡</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => openComments()}>
+          <Text>💬</Text>
+        </TouchableOpacity>
+      </View>
+      <Text>{title}</Text>
+      <Text>{content}</Text>
+
+      <Modal
+        visible={isCommentModalOpen}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={openComments}
+      >
+        <View>
+          <View>
+            <Text>Comments</Text>
+            <ScrollView>
+              {comments.map((comment, index) => (
+                <View>
+                  <Image source={{ uri: userAvatar }}/>
+                  <Text key={index}>{comment.content}</Text>
+                  <Text>{createdAt}</Text>
+                </View>
+              ))}
+            </ScrollView>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  ); 
+};
 
 export default PostCard;
