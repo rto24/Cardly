6a7450384c327e7d0ff31e0ba911d57d0f939d2f
rto24 <richardto180@gmail.com>
2024-12-25 13:55:31 -0500

    imported global css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { SafeAreaView } from 'react-native';
 import AppNav from './navigation/AppNav';
 import { AuthProvider } from './context/AuthContext';
+import "./global.css";
 
 export default function App() {
   return (
