fbcfbb10296c1969facfcdb09bd2cbb9bbec6394
rto24 <richardto180@gmail.com>
2024-12-31 02:33:05 -0500

    edit post routes to include auth middleware

--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response } from "express";
 import { createPost, getUserPosts, likePost, commentOnPost, getPostLikes, getComments } from "../controllers/postController";
+import { authenticateJWT } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
-router.get("/:userId", getUserPosts);
-router.post("/create-post", createPost);
-router.post("/:postId/like", likePost);
-router.post("/:postId/comment", commentOnPost);
-router.get("/:postId/like", getPostLikes);
-router.get("/:postId/comment", getComments);
+router.get("/:userId", authenticateJWT, getUserPosts);
+router.post("/create-post", authenticateJWT, createPost);
+router.post("/:postId/like", authenticateJWT, likePost);
+router.post("/:postId/comment", authenticateJWT, commentOnPost);
+router.get("/:postId/like", authenticateJWT, getPostLikes);
+router.get("/:postId/comment", authenticateJWT, getComments);
 
 export default router;
 
