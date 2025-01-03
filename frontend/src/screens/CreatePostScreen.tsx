d334f18b2c8d52940455299ab3eb16304eeee41b
rto24 <richardto180@gmail.com>
2025-01-03 01:42:47 -0500

    added function to handle post creation

--- frontend/src/screens/CreatePostScreen.tsx
+++ frontend/src/screens/CreatePostScreen.tsx
@@ -1,7 +1,30 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { createPost } from '../services/postService';
+import { useAuth } from '../context/AuthContext';
 
 function CreatePostScreen() {
+  const [ title, setTitle ] = useState<string>("");
+  const [ content, setContent ] = useState<string>("");
+  const [ image, setImage ] = useState<string>("");
+  const [ errorMessage, setErrorMessage ] = useState<string | null>(null);
+
+  const { user } = useAuth();
+
+  const handleCreatePost = async () => {
+    if (!title || !content || !image || !user) {
+      setErrorMessage("Required field is missing");
+      return;
+    }
+
+    try {
+      await createPost(title, content, image, user);
+      setErrorMessage(null);
+    } catch (error: any) {
+      setErrorMessage(error)
+    }
+  };
+
   return (
     <View>
       <Text>CreatePostScreen</Text>
