ae553ad038ecaf0b1c7b199b4cb0a403c69a6055
rto24 <richardto180@gmail.com>
2025-01-05 01:57:37 -0500

    conditionally loaded bottom navbar upon successful user authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import { SafeAreaView } from 'react-native';
 import AppNav from './navigation/AppNav';
-import { AuthProvider } from './context/AuthContext';
+import LoginScreen from './screens/LoginScreen';
+import { AuthProvider, useAuth } from './context/AuthContext';
 import "./global.css";
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <AuthProvider>
-        <AppNav />
+        <MainNav />
       </AuthProvider>
     </SafeAreaView>
   );
+}
+
+const MainNav = () => {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <AppNav /> : <LoginScreen />
 }
