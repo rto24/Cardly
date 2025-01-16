ae2a2af9fdbc7adc2d106882c23a678716682c6b
rto24 <richardto180@gmail.com>
2025-01-16 18:55:43 -0500

    method to fetch all available servers

--- backend/src/controllers/serverController.ts
+++ backend/src/controllers/serverController.ts
@@ -14,3 +14,12 @@ export const createServer = async (req: Request, res: Response): Promise<void> =
     res.status(500).json({ message: "Error creating post:", error });
   }
 };
+
+export const getServers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const servers = await prisma.server.findMany();
+    res.status(200).json(servers);
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving available serveres:", error });
+  }
+};
