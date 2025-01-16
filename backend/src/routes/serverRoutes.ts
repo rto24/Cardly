949888d7ebe11143276e75b1271141c98e3ae42a
rto24 <richardto180@gmail.com>
2025-01-16 18:56:15 -0500

    added route for get servers method

--- backend/src/routes/serverRoutes.ts
+++ backend/src/routes/serverRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { createServer } from "../controllers/serverController";
+import { createServer, getServers } from "../controllers/serverController";
 import { authenticateJWT } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
+router.get("/available-servers", getServers);
 router.post("/create-server", authenticateJWT, createServer); 
 
 export default router;
