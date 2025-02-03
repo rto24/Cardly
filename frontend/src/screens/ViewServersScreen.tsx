1ecb8cb8c4857ff8037d2877cd1853241b0d5cb8
rto24 <richardto180@gmail.com>
2025-02-03 18:09:52 -0500

    fetched servers using server service

--- frontend/src/screens/ViewServersScreen.tsx
+++ frontend/src/screens/ViewServersScreen.tsx
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Servers } from "../types/types";
 import { View, Text } from "react-native";
+import { getServers } from "../services/serversService";
 
 const ViewServersScreen = ({ navigation }: any) => {
   const [ servers, setServers ] = useState<Servers[]>([]);
 
+  useEffect(() => {
+    const loadServers = async () => {
+      const servers = await getServers();
+      setServers(servers);
+    }
+    loadServers();
+  }, []);
+
   return (
     <View>
       <Text>Join a Server</Text>
+      {servers.map((server, index) => (
+        <View key={index}>
+          <Text>{server.name}</Text>
+        </View>
+      ))}
     </View>
   )
 }
